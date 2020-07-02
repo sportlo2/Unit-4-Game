9df656751fe271bebf98e71a1bfd5111931fbbda
sportlo2 <portlock.sierra@gmail.com>
2020-07-02 18:50:52 -0400

    psuedo code js

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// generate random number 1-50
+
+// display 4 crystals and assign a number to it. 
+// Add click functions that updates total value. 
+// Value of each crystal should be between 1-15
+
+// *****Ending game
+
+// update "Wins:" if true:
+    // total score matches randomly generated number
+
+// update "Losses" if total score is above randomly generated number
+
+// If total score is equal to or above generated number then resetart game

0eebc7a5f23920d2c1b90816084fd0125c739092
sportlo2 <portlock.sierra@gmail.com>
2020-07-12 18:40:48 -0400

    defined var

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,33 @@
-// generate random number 19-20
+var wins = 0;
+var losses = 0;
+var Score = 0;
+var generateNumber = 0;
 
-// display 4 crystals and assign a number to it. 
-// Add click functions that updates total value. 
-// Value of each crystal should be between 1-12
+//counter
+var gemOne = 0;
+var gemTwo = 0;
+var gemThree = 0;
+var gemFour = 0;
+
+
+// generate random number 15-45
+$("")
+// display 4 crystals and assign a random number to it. 
+// Value of each crystal should be between 1-12.
+
+// when click a crystal total score counter updates by crystal random assigned number
 
 // *****Ending game
 
 // update "Wins:" if true:
     // total score matches randomly generated number
+    // then alert "You Win"
+    // update win counter
+    //game starts over
 
 // update "Losses" if total score is above randomly generated number
+// then alert "You Lose"
+// update loss counter
+// game starts over
 
 // If total score is equal to or above generated number then resetart game
